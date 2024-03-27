0b1334d7fcfe8aa91431e416a39ac35d1885cb8e
StjeanPhil <philippe_spellerin@hotmail.ca>
2024-03-27 16:46:54 -0400

    Walmart update
    Found a path to solve Walmart
    did not find the complete solution

--- walmartCrawl.py
+++ walmartCrawl.py
@@ -1,82 +1,122 @@
-import json
-#import indigoCrawl
-import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import json,re,time
 from bs4 import BeautifulSoup
-from fake_useragent import UserAgent
-import datetime
-import re
+from datetime import datetime
+from selenium.webdriver.common.action_chains import ActionChains
 
-import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+import undetected_chromedriver as uc 
 
 
 
-def main():
 
-    url_root= "https://www.walmart.ca/en/browse/toys/lego/10011_6000205043132?icid=landing%2Fcp_page_toys_shop_all_lego_18944_EU28ISZ2DN&facet=f_SellerType_en%3AWalmart%7C%7Cbrand%3ALEGO%7C%7Cf_Availability_en%3AOnline"
-    #url_root= "https://www.walmart.ca"
+#Walmart has a bot detection
+#It has a json_blob containing all the info abt the products in the current page
+#Selenium is only used to move between pages, maybe it could be done only by sending a request per page.
+# Selinium might be blocked by the bot detection, try using better headers and proxies
+
+
+def scanSite(driver,data):
+
+        site = driver.find_element(By.XPATH,"//body").get_attribute('outerHTML')
+        soup = BeautifulSoup(site, "html.parser")
+
+        #find the data of the page
+        script_tag = soup.find("script", {"id": "__NEXT_DATA__"})
     
-    ua = UserAgent()
-    #call the UserAgent as ua
-    userAgent = ua.random
-    # make it random
-    headers = {"User-Agent": userAgent} 
-    working= True
-    page=0
-    #list of products
+        if script_tag is not None:
+            json_blob = json.loads(script_tag.get_text())
+            product_list = json_blob["props"]["pageProps"]["initialData"]["searchResult"]["itemStacks"][0]["items"]
+    
+    
+        for tile in product_list:
+
+            #make sure the tile is a product
+            if not tile['__typename']=="Product": continue
+
+            # Find the price, name ,date and link of the product
+            name = tile['name']
+            try:                                                                                        
+                name=re.findall(r'\b\d{5}\b', name)[0]                          #clean it 
+            except Exception as error:
+                print(error)
+                print("error in : "+name)
+    
+            price =tile["priceInfo"]["linePrice"]
+            price=int(price.replace(".","").replace("$",""))  #clean it
+    
+            href="https://www.walmart.ca"+tile["canonicalUrl"]
+    
+            date = datetime.now().strftime('%Y-%m-%d-%H')  # get the current date
+            inStock=tile["availabilityStatusV2"]
+            # Create a dictionary with the scraped data
+            product = {
+                'name': name,   #Series number
+                'price': price, #Price in cents
+                'href': href,  #Link to the product
+                'date': date,    #Data's date
+            }
+    
+            # Append the dictionary to the list
+            data.append(product)
+            
+        #If there is a next page button, click it and scan the site again
+        nextPage = driver.find_element(By.XPATH,"//a[@data-testid()='NextPage']")
+        if nextPage:
+            actions = ActionChains(driver)#initialize the action chain=
+            actions.move_to_element(nextPage).click().perform() 
+            time.sleep(3)
+            scanSite(driver,data)
+            
+
+
+def crawl():
+    
+    url ='https://www.walmart.ca/en/search?q=lego&facet=f_SellerType_en%3AWalmart%7C%7Cf_Availability_en%3AOnline%7C%7Cbrand%3ALEGO&catId=10011_6000205043132'
+    #initialize the browser
+    # Create Chromeoptions instance 
+    options = webdriver.ChromeOptions() 
+    
+    # Adding argument to disable the AutomationControlled flag 
+    options.add_argument("--disable-blink-features=AutomationControlled") 
+    
+    # Exclude the collection of enable-automation switches 
+    options.add_experimental_option("excludeSwitches", ["enable-automation"]) 
+    
+    # Turn-off userAutomationExtension 
+    options.add_experimental_option("useAutomationExtension", False) 
+    
+    # Setting the driver path and requesting a page 
+    driver = uc.Chrome() 
+    
+    # Changing the property of the navigator value for webdriver to undefined 
+    #driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})") 
     data=[]
-    while working:
-        working=False
-        page+=1
-        #url=url_root+"&page="+str(page)+"&affinityOverride=default"
-        #url=url_root+"&affinityOverride=default"
-        url=url_root
-        soup = BeautifulSoup(requests.get(url, headers=headers).content, 'html.parser')
-        print(soup)
-        tiles = soup.body.find_all('div', {'data-testid': 'list-view'})
-        print(len(tiles))
+    #go to the website
+    driver.get(url)
+    time.sleep(4)
+    #accept cookies
+    element = driver.find_element(By.XPATH, "//button[text()='Accept all']")
+    
+    actions = ActionChains(driver)#initialize the action chain=
+    actions.move_to_element(element).click().perform()
+    time.sleep(2)
+    #scan the site, if more pages, it recursively calls itself to scan the next page
+    scanSite(driver,data)
+    driver.quit()
+    
 
-        
-        
-        for tile in tiles:
-            #working=True
-            try:
-                price=int(tile.find('span',class_='w_q67L').contents[0].split('$')[1].replace('.','').replace(',',''))
-                name=tile.find('span',{'data-automation-id':'product-title'}).contents[0]
-                name=re.findall(r'\b\d{5}\b', name)[0]
-                #date = datetime.now().strftime('%Y-%m-%d')  # get the current date
-                #find the "a" element of the sibling element of tile
 
-                href =tile.find_previous_sibling('a').get('href')
-                #print(href)
-
-                # Create a dictionary with the scraped data
-                product = {
-                    'name': name,   #Series number
-                    'price': price, #Price in cents
-                #    'date': date,    #Data's date
-                    'link': href
-                }
-                # Append the dictionary to the list
-                data.append(product)
-            except:
-                continue
-            
-        print(len(data))
-
-            
-        #save the scraped data to a json file
-    print(len(data))
     with open('./data/WalmartCurrent.json', 'w') as outfile:
-        json.dump(data, outfile)
-        print('Walmart sales data has been scraped and saved to ./data/WalmartCurrent.json')
-        outfile.close()
-
-
-
+            json.dump(data, outfile)
+            print('Walmart sales data has been scraped and saved to ./data/IndigoCurrent.json')
+            outfile.close()
+    return data
 
 
 
 if __name__ == "__main__":
-    main()
-
-    
+    crawl()
